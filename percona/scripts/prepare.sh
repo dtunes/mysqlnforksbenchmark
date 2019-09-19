3eac00f66e61f49a00b0208cd5052cbe44f014e6
diegotdai <diegotdai@gmail.com>
2019-09-20 11:25:10 +1200

    Initial commit

---
+++ percona/scripts/prepare.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+sysbench --db-driver=mysql \
+    --mysql-table-engine=innodb \
+    --mysql-user=sysbench \
+    --mysql-db=sysbench \
+    --mysql-password=sysbench \
+    --range_size=100 \
+    --table_size=1000000 \
+    --tables=1 \
+    --threads=8 \
+    --events=0 \
+    --time=60 \
+    --rand-type=uniform \
+    /usr/share/sysbench/oltp_read_write.lua prepare
